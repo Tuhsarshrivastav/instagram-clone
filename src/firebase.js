f5b98742e4691456b194c5cfdbe6452ea4882798
Tushar Shrivastav <74534919+Tuhsarshrivastav@users.noreply.github.com>
2021-02-07 09:26:37 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyDh9vxkQu-0xpQSAkyIAV7odbv8FYwjw0I",
-  authDomain: "instagram-clone-27901.firebaseapp.com",
-  projectId: "instagram-clone-27901",
-  storageBucket: "instagram-clone-27901.appspot.com",
-  messagingSenderId: "877153298318",
-  appId: "1:877153298318:web:442335ab781731759026df",
-  measurementId: "G-W2EEBZ2RJG",
+// paste your firebase cdn 
 });
 
 const db = firebaseApp.firestore();
